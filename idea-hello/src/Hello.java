b1937506d0062913a3c9892daa9549fc3e0579e2
hp <1142925618@qq.com>
2020-07-21 21:10:58 +0800

    修改文件

--- idea-hello/src/Hello.java
+++ idea-hello/src/Hello.java
@@ -1,9 +1,13 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        System.out.println("hello3");
     }
 
     public void test(){
         System.out.println("jjjj");
     }
+
+
 }
